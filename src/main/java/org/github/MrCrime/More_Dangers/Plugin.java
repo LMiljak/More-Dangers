c6bad431aa6b82278b95194416c9360d2980103c
Luka Miljak <lmiljak@student.tudelft.nl>
2015-08-07 20:08:07 +0200

    Third commit
    Main plugin class added and plugin.yml file added.

---
+++ src/main/java/org/github/MrCrime/More_Dangers/Plugin.java
@@ -0,0 +1,15 @@
+package org.github.MrCrime.More_Dangers;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * The "main" class of the plugin.
+ */
+public class Plugin extends JavaPlugin {
+    
+	@Override
+	public void onEnable() {
+		
+	}
+	
+}
